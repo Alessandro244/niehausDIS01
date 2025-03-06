5c14b6d391d008b6d6986a94c65d98240d04c8ed
Alessandro244 <alessandro.fiorello@smail.th-koeln.de>
2025-03-06 19:31:27 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
+       // Das Hamburger-Menü
+       const hamburger = document.getElementById("afiorello_hamburger_button");
+       const navbar = document.getElementById("afiorello_navigationsleiste");
+   
+       // Event Listener für den Hamburger-Button
+       hamburger.addEventListener("click", () => {
+           navbar.classList.toggle("aktiv");
+       });
 
