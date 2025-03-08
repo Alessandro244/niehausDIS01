7dd831cd08a15fb65580e10c420cc74d81cc9809
Alessandro244 <alessandro.fiorello@smail.th-koeln.de>
2025-03-08 12:55:54 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-       // Das Hamburger-Menü
-       const hamburger = document.getElementById("afiorello_hamburger_button");
-       const navbar = document.getElementById("afiorello_navigationsleiste");
+       // Hamburger-Menü
+       const hamburger = document.getElementById("afiorello_hamburger_button"); // Sucht das HTML-Element (button in der navbar) mit der ID "afiorello_hamburger_button" und speichert es in der Variable hamburger
+       const navbar = document.getElementById("afiorello_navigationsleiste"); // Sucht das HTML-Element (ul in der navbar) mit der ID "afiorello_navigationsleiste" und speichert es in der Variable navbar
    
        // Event Listener für den Hamburger-Button
-       hamburger.addEventListener("click", () => {
-           navbar.classList.toggle("aktiv");
+       hamburger.addEventListener("click", () => { // Reagiert auf einen Klick des Users auf den Button innerhalb der navbar bei kleinen Bildschirmbreiten
+           navbar.classList.toggle("aktiv"); // Fügt der ul in der navbar die Klasse "aktiv" zu oder entfernt sie, um das Aufklappen der einspaltigen navbar per CSS bei kleinen Bildschirmbreiten sichtbar zu machen
        });
 
